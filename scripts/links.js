6a8ecbc1f33d93ac73fe4e2ca54274e3fd6069e1
dev-juve <juvenson101@gmail.com>
2024-07-19 08:57:33 -0400

    links.json updated

--- scripts/links.js
+++ scripts/links.js
@@ -3,18 +3,21 @@ const linksURL = "https://dev-juve.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     try {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    if (data.ok) {
-        console.table(data);
-    }
-    else {
-        throw Error(await response.text());
-    }
-    }
-
-    catch (error) {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            // console.table(data);
+            displayLinks(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
         console.log(error);
     }
+}
+
+getLinks();
+
+const displayLinks = (weeks) => {
     
 }
